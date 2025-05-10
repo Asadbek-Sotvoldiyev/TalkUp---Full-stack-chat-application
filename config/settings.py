60ccee9087477c1839f46f5f6808ebd7ac4b37b0
Asadbek Sotvoldiyev <blogasadbek@gmail.com>
2025-05-10 11:38:51 +0500

    Messages has encrypted

--- config/settings.py
+++ config/settings.py
@@ -1,24 +1,27 @@
 from pathlib import Path
-
+from dotenv import load_dotenv
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = os.getenv('DEBUG')
 
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 ALLOWED_HOSTS = ['*']
 
-FERNET_KEY = config('FERNET_KEY')
+FERNET_KEY = os.getenv('FERNET_KEY')
 
 
 # Application definition
